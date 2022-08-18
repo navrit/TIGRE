e0b85b264dbbe6e9a237b0bfcbf520d88a1acab8
Navrit Bal <n.bal@clin.au.dk>
2022-08-18 16:58:58 +0200

    Now it actually works

--- Python/tigre/__init__.py
+++ Python/tigre/__init__.py
@@ -1,12 +1,18 @@
 from __future__ import division, absolute_import, print_function
-from .utilities.geometry import geometry
-from .utilities.geometry_default import ConeGeometryDefault as geometry_default
-from .utilities.Ax import Ax
-from .utilities.Atb import Atb
-from .utilities.visualization.plotproj import plotproj, plotProj, plotSinogram
-from .utilities.visualization.plotimg import plotimg, plotImg
-from .utilities.visualization.plot_geometry import plot_geometry
-from .utilities.visualization.plot_angles import plot_angles
-from .utilities.CTnoise import add
-from .utilities.common_geometry import staticDetectorGeo, staticDetLinearSourceGeo
+import os
+import sys
+from .utilities.data_loader import load_medipix3_files2 as load_medipix
+from .utilities.data_loader import load_rayence_projections as load_rayence
 from . import algorithms
+from .utilities.common_geometry import staticDetectorGeo, staticDetLinearSourceGeo
+from .utilities.CTnoise import add
+from .utilities.visualization.plot_angles import plot_angles
+from .utilities.visualization.plot_geometry import plot_geometry
+from .utilities.visualization.plotimg import plotimg, plotImg
+from .utilities.visualization.plotproj import plotproj, plotProj, plotSinogram
+from .utilities.Atb import Atb
+from .utilities.Ax import Ax
+from .utilities.geometry_jasper import ConeGeometryJasper as geometry_jasper
+from .utilities.geometry_default import ConeGeometryDefault as geometry_default
+from .utilities.geometry import geometry
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
