4cd05ce70b38a8d036b42dfdf9f7960cf9930d51
Navrit Bal <n.bal@clin.au.dk>
2022-08-24 16:42:31 +0200

    Adapted gReconParams for correct recons

--- Python/demos/recon_parameter_scanning.py
+++ Python/demos/recon_parameter_scanning.py
@@ -23,6 +23,8 @@ import os
 
 import nibabel as nib
 import numpy as np
+from PIL import Image
+from tqdm import trange
 
 import shared_functions as s
 
@@ -32,9 +34,15 @@ if __name__ == "__main__":
     2. Scan over a parameter with one reconstruction each
 
     '''
+    scan_over_a_paramter = False
+    th = 0
+
     # Make folders and load files for recon
     drive = 'f:\\'
     base_folder = os.path.join(drive, 'jasper', 'data', '20220822_Al_Phantom_Recon_Alignment')
+    # 20220822_ffpe_WhateverBreast        .... FILL ME IN ....                                        a = 1.2
+    # 20220822_Al_Phantom_Recon_Alignment det_rot=(0 to -0.5, 0, 0) x=5.02 y=-0.24                    a =
+
     base_json_file = os.path.join(base_folder, 'scan_settings.json')
     output_folder = os.path.join(base_folder, 'parameter_scanning')
     if not os.path.exists(output_folder):
@@ -56,54 +64,54 @@ if __name__ == "__main__":
     gReconParams['pixels'] = 512  # (pixels)
     gReconParams['pixel_pitch'] = 0.055  # (mm)
     gReconParams['recon_voxels'] = (
-        gReconParams['pixels'], gReconParams['pixels'], gReconParams['pixels'])  # number of voxels (vx)
-    gReconParams['recon_size'] = (gReconParams['pixels'] * gReconParams['pixel_pitch'], gReconParams['pixels']
-                                  * gReconParams['pixel_pitch'], gReconParams['pixels'] * gReconParams['pixel_pitch'])  # (mm)
-
-    ''' TODO These should really be read from the JSON file! '''
+        gReconParams['pixels'], gReconParams['pixels'], gReconParams['pixels'])  # number of voxels (vx) # TODO Might be less than 1/2 or 1/4 ... of the number of pixels
     gReconParams['distance_source_detector'] = 188.347  # 9+9+30+100+30+9+1.347 (mm)
-    gReconParams['z_stage_distance_mm'] = 20  # Varies between 0 and 100 mm
-    gReconParams['distance_object_detector'] = 30 + \
-        gReconParams['z_stage_distance_mm'] + 9+1.347  # (mm)
-    gReconParams['detector_rotation'] = (math.radians(-0.3), 0., 0.)  # (mm)
+    gReconParams['z_stage_distance_mm'] = s.get_sample_z_from_first_scan_json(
+        base_json_file)  # Varies between 0 and 100 mm
+    gReconParams['distance_object_detector'] = gReconParams['z_stage_distance_mm'] + \
+        30+9+1.347  # (mm)
 
-    centre_of_rotation_offset_y_mm = 2.51  # (mm)
+    # 1.2  # gReconParams['pixels'] * gReconParams['pixel_pitch']
+    # a = gReconParams['pixels'] * gReconParams['pixel_pitch'] / (gReconParams['distance_source_detector'] / (
+    #     gReconParams['distance_source_detector']-gReconParams['distance_object_detector'])) / 10
+    # a = gReconParams['pixel_pitch']
+    DSD = gReconParams['distance_source_detector']
+    DSO = DSD - gReconParams['distance_object_detector']
+    a = 2 * 512 * 0.055 / (((DSD-DSO) / DSO) + 1)
+
+    gReconParams['recon_size'] = (a, a, a)  # 28.16 (mm)
+    gReconParams['detector_rotation'] = (math.radians(0.), 0., 0.)  # (mm)
+
+    assert gReconParams['z_stage_distance_mm'] < 100 and gReconParams['z_stage_distance_mm'] > 0
+    print(gReconParams)
+
+    centre_of_rotation_offset_x_mm = 2.51
+    centre_of_rotation_offset_y_mm = -0.24  # Could be 0, not sure yet
 
     ofc_bpc = s.save_and_or_load_npy_files(
-        output_folder, 'bpc.npy', lambda: s.generate_bad_pixel_corrected_array(ofc, gReconParams))
+        output_folder, f'th{th}_bpc.npy', lambda: s.generate_bad_pixel_corrected_array(ofc, gReconParams))
 
-    # r = np.linspace(-1, 1, 21)
-    # # unit = 'mm'
-    # unit = 'degrees'
-    # for i in trange(0, len(r)):
-    #     gReconParams['detector_rotation'] = (math.radians(
-    #         r[i]), 0., 0.)  # (mm) TODO Check accuracy!!!!!
-    #     # centre_of_rotation_offset_y_mm = r[i]
+    if scan_over_a_paramter:
+        r = np.linspace(1, 4, 31)
+        unit = 'mm'
+        # unit = 'degrees'
+        for i in trange(0, len(r)):
+            # gReconParams['detector_rotation'] = (math.radians(r[i]), 0., 0.)
+            # centre_of_rotation_offset_y_mm = r[i]
+            centre_of_rotation_offset_x_mm = r[i]
 
-    #     # a = 400
-    #     # img_th0 = recon_scan(gReconParams, ofc_bpc[:, a:a+11, :], angles, z_offset,
-    #     #                      detector_x_offsets, detector_y_offsets, centre_of_rotation_offset_y_mm)
+            a = 200
+            img_th0, geo = s.recon_scan(gReconParams, ofc_bpc[:, a:a+11, :], angles,
+                                        detector_x_offsets, detector_y_offsets, centre_of_rotation_offset_x_mm, centre_of_rotation_offset_y_mm, False)
+            # img_th0, geo = s.recon_scan(gReconParams, ofc_bpc[:, :, :], angles,  detector_x_offsets, detector_y_offsets, centre_of_rotation_offset_y_mm, True)
 
-    #     img_th0 = recon_scan(gReconParams, ofc_bpc[:, :, :], angles, z_offset,
-    #                          detector_x_offsets, detector_y_offsets, centre_of_rotation_offset_y_mm)
+            b = 512//2
+            img = Image.fromarray(img_th0[:, :, b])
+            img.save(os.path.join(output_folder,
+                                  '{:s}_{:0.4f}_{:s}.tiff'.format(str(i).zfill(6), r[i], unit)))
+    else:
+        img_th0, geo = s.recon_scan(gReconParams, ofc_bpc, angles, detector_x_offsets,
+                                    detector_y_offsets, centre_of_rotation_offset_x_mm, centre_of_rotation_offset_y_mm, True)
 
-    # ni_img = nib.Nifti1Image(img_th0, np.eye(4))
-    # save_array(output_folder, f'Proj_th0_{r[i]}_{unit}.nii', ni_img)
-
-    # b = 512//2
-    # img = Image.fromarray(img_th0[:, :, b])
-    # img.save(os.path.join(output_folder,
-    #          '{:s}_{:0.4f}_{:s}.tiff'.format(str(i).zfill(6), r[i], unit)))
-
-img_th0 = s.recon_scan(gReconParams, ofc_bpc[:, :, :], angles, z_offset,
-                       detector_x_offsets, detector_y_offsets, centre_of_rotation_offset_y_mm)
-ni_img = nib.Nifti1Image(img_th0, np.eye(4))
-s.save_array(output_folder, 'Al_Phantom.nii', ni_img)
-# ni_img = nib.Nifti1Image(ofc_bpc, np.eye(4))
-# save_array(output_folder, f'Proj_ofc_bpc_{0}_{unit}.nii', ni_img)
-
-# img_th0 = recon_scan(gReconParams, ofc_bpc, angles, z_offset, detector_x_offsets,
-#                         detector_y_offsets, global_detector_shift_y)
-
-# ni_img = nib.Nifti1Image(img_th0, np.eye(4))
-# save_array(output_folder, f'Proj_th0_{0}_{unit}.nii', ni_img)
+        ni_img = s.make_Nifti1Image(img_th0, geo.dVoxel)
+        s.save_array(output_folder, f'Proj_th0_manual.nii', ni_img)
